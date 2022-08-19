cfa9a95fd1f70565a009938b20fa1ab755ba1963
aftabmaner-27 <aftabmaner3131@gmail.com>
2022-08-19 11:23:52 +0530

    Value increment and decrement Demo

--- app/src/main/java/com/aftabmaner/qtyincrementanddecrement/MainActivity.java
+++ app/src/main/java/com/aftabmaner/qtyincrementanddecrement/MainActivity.java
@@ -8,26 +8,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-     TextView value;
-     int count = 0 ;
+    TextView value;
+    int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         value = findViewById(R.id.value);
-
-
     }
-    public void increment(View view){
+
+    public void increment(View view) {
         count++;
-        value.setText(""+count);
-
-
+        value.setText("" + count);
     }
-    public void decrement(View view){
-       if (count <= 0) count = 0;
-       else count--;
-        value.setText(""+count);
+
+    public void decrement(View view) {
+        if (count <= 0) count = 0;
+        else count--;
+        value.setText("" + count);
     }
 }
